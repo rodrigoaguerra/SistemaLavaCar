46db9f57ae9df8a7ed08798881b3f41bc8369996
Gabriel Eugenio <gabriel.eug2@gmail.com>
2016-04-11 19:25:14 -0300

    +Adicionando ano e simplificando as mensagens

--- Sistema LavaCar/src/sistema/lavacar/Vehicles.java
+++ Sistema LavaCar/src/sistema/lavacar/Vehicles.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sistema.lavacar;
 
 import java.io.Serializable;
@@ -10,41 +6,42 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-/**
- *
- * @author rodrigo
- */
 public class Vehicles implements Serializable {
     public String board, 
                   brand,
-                  color,
                   model,
-                  dateOfInsert,
-                  description;
-    
-    SimpleDateFormat dateToday;
+                  year,
+                  color,
+                  description,
+                  dateOfInsert;
     public int  size;
     
+    SimpleDateFormat dateToday;
+
     public Vehicles(){
         Scanner input = new Scanner (System.in);
 
-        System.out.print("Placa Do Veículo .: ");
+        System.out.ptinln("\tInformaçoes do veiculo");
+        System.out.print("Placa .: ");
         board = input.nextLine();
 
-        System.out.print("Marca Do Veículo .: ");
+        System.out.print("Marca .: ");
         brand = input.nextLine();
-
-        System.out.print("Cor Do Veículo .: ");
-        color = input.nextLine();
-
-        System.out.print("Modelo Do Veículo .: ");
+        
+        System.out.print("Modelo .: ");
         model = input.nextLine();
         
-        System.out.print("Decrição Do Veículo .: ");
-        description = input.nextLine();
+        System.out.ptin("Ano .: ");
+        year = input.nextLine();
         
-        System.out.print("Digete 1 Para Veículo Pequeno, 2 Para Veículo Médio e 3 Para Grande.");
-        size = input.nextInt();
+        System.out.print("Cor .: ");
+        color = input.nextLine();
+
+        System.out.print("Decrição .: ");  
+        description = input.nextLine();  
+        
+        System.out.print("Tamanho (1 - Pequeno, 2 - Médio, 3 - Grande) .: ");  
+        size = input.nextInt();  
         
         dateToday = new SimpleDateFormat("dd/MM/yyyy hh:mm");
         dateOfInsert = dateToday.format(new Date()); 
