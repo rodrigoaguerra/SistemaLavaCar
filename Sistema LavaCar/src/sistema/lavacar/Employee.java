1e9d2017feb7f063dbd3d7d9df9f29cbf4f010b2
Gabriel Eugenio <gabriel.eug2@gmail.com>
2016-04-12 20:17:26 -0300

    +Relatórios de clientes, veículos e funcionários

--- Sistema LavaCar/src/sistema/lavacar/Employee.java
+++ Sistema LavaCar/src/sistema/lavacar/Employee.java
@@ -19,9 +19,20 @@ public class Employee  extends Person implements Serializable{
         Scanner input = new Scanner (System.in);
 
         System.out.print("Data De Aniversário .: ");
-        dateOfBorn = input.nextLine();
+        dateOfBirth = input.nextLine();
 
         System.out.print("CPF .: ");
         cpf = input.nextLine();
+    }
+    public String gerarRelatorio()
+    {
+        String rel = "";
+        rel = rel + "Nome: " + name + "\tRG: " + rg + "\tData de nascimento: " +
+                dateOfBirth + "\n";
+        rel = rel + "Telefone 1: " + phone1 + "\tTelefone 2: " + phone2 + "\n";
+        rel = rel + "Endereço: " + address + "\n";
+        rel = rel + "Data de aniversario: " + dateOfBirth + "\tData de contratação: " +
+                dateOfInsert;
+        return rel;
     }
 }
