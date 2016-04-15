6b0eed32f0b4987e0237bbdb9237f74e9c6a45ab
Gabriel Eugenio <gabriel.eug2@gmail.com>
2016-04-15 10:02:48 -0300

    adicionaVeiculo()
    Método que adiciona mais um veículo no ArrayList. Pode acessar os getters e setters da classe Person por herdar as características dela.

--- Sistema LavaCar/src/sistema/lavacar/Customers.java
+++ Sistema LavaCar/src/sistema/lavacar/Customers.java
@@ -42,19 +42,4 @@ public class Customer extends Person implements Serializable {
         Vehicle vehicle = new Vehicle();
         vehiclesOfCustomer.add(vehicle);
     }
-    public void setAddress(String address)
-    {
-        this.address = address;
-    }
-    public void setPhone1(String phone1)
-    {
-        this.phone1 = phone1;
-    }
-    public void setPhone2(String phone2)
-    {
-        this.phone2 = phone2;
-    }
-    public String getName() {
-        return name;
-    }
 }
