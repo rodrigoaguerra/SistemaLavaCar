53ce45b4073a3b03fbd1fd180dbc38ca075fca90
Gabriel Eugenio <gabriel.eug2@gmail.com>
2016-04-15 10:08:08 -0300

    Main só chama métodos da Company
    Ficou mais simples, agora é só um switch que chama métodos como menuPrincipal, menuCliente, etc.

--- Sistema LavaCar/src/sistema/lavacar/SistemaLavaCar.java
+++ Sistema LavaCar/src/sistema/lavacar/SistemaLavaCar.java
@@ -14,158 +14,66 @@ import java.util.Scanner;
  * @author Rodrigo Alves Guerra, Gabriel Eugenio Brito, Caio
  */
 public class SistemaLavaCar {
-    private String name_company;
-    private String address_company;
-    private String phone_company;
-    private String site_company;
-    private String cnpj; 
-   /* private String logo_company; Não seria slogan? */
-      
-    /*
-    Pensei em fazer uma construtora para a classe principal, dando os valores
-    da companhia (nome, phone, cnpj...), mas não deu certo.
-    
-    public SistemaLavaCar()
-    {
-        Scanner input = new Scanner (System.in);
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner input = new Scanner(System.in);
         
         System.out.println("\tINFORMACOES DA EMPRESA");
         System.out.print("Nome: ");
-        name_company = input.nextLine();
+        String name = input.nextLine();
         System.out.print("Endereço: ");
-        address_company = input.nextLine();
+        String address = input.nextLine();
         System.out.print("Telefone: ");
-        phone_company = input.nextLine();
+        String phone = input.nextLine();
         System.out.print("Site: ");
-        site_company = input.nextLine();
+        String site = input.nextLine();
         System.out.print("CNPJ: ");
-        cnpj = input.nextLine();
-    }*/
-    public static void main(String[] args) throws FileNotFoundException{
-        Scanner input = new Scanner (System.in);
-        
-        ArrayList<Customers> people = new ArrayList<>();
-        ArrayList<Employee> employees  = new ArrayList<>();
+        String cnpj = input.nextLine();
 
+        Company c = new Company(name, address, phone, site, cnpj);
+                        
+        ArrayList<Customer> people = new ArrayList<>();
+        ArrayList<Employee> employees  = new ArrayList<>();
+        
         // Lê as informações dos clientes, funcionários
-        people = (ArrayList<Customers>) WorkingFile.read(people, "customers");
+        people = (ArrayList<Customer>) WorkingFile.read(people, "customers");
         employees = (ArrayList<Employee>) WorkingFile.read(employees, "employees");
         
-        int answer = 0, //Reposta do menu principal
-            answer2; //Resposta dos sub-menus
-        String name, rel;
+        int answer = 0; //Reposta do menu principal
+        boolean volta;
         do{
             switch(answer){
                 case 1 : // Gerenciamento de clientes
-                    System.out.println("\tCLIENTES");
-                    System.out.println("(1) Cadastrar um novo cliente");
-                    System.out.println("(2) Listar clientes cadastrados");
-                    System.out.println("(0) Voltar ao menu principal");
-                    answer2 = input.nextInt();
-                    switch(answer2){
-                        case 1:
-                            Customers person = new Customers(); 
-                            people.add(person);
-                            System.out.println("Cliente cadastrado com sucesso!");
-                            break;
-                        case 2:
-                            System.out.println("\tLISTA DE CLIENTES");
-                            /* Aqui poderíamos imprimir só o nome, para parecer
-                            mais com uma lista. Afinal, todas as informações vão
-                            ficar salvas no relatório depois. */
-                            for(Customers p : people){
-                                rel = p.gerarRelatorio();
-                                System.out.println(rel);
-                                }
-                            break;
-                        case 0:
-                            answer = 9;
-                            break;
-                        default:
-                            System.out.println("Opcao invalida.");
-                    }
+                    volta = c.menuCliente(people);
+                    if(volta == true)
+                        answer = 10;
                     break;
                 case 2: //Gerenciamento de veículos
-                    System.out.println("\tVEICULOS");
-                    System.out.println("(1) Cadastrar veículo em um cliente já existente");
-                    System.out.println("(2) Listar veículos de um cliente");
-                    System.out.println("(0) Voltar ao menu principal");
-                    answer2 = input.nextInt();
-                    switch(answer2){
-                        case 1:
-                            System.out.print("Digite o nome do proprietario: ");
-                            name = input.nextLine();
-                            /* Procura o nome no Arraylist. Não tenho certeza
-                            como fazer isso ainda.
-                            Depois cria um novo Vehicle, pede as informações
-                            e guarda no Arraylist de veículos do cliente. */
-                            System.out.println("Veículo cadastrado com sucesso!");
-                            break;
-                        case 2:
-                            System.out.print("Digite o nome do proprietario: ");
-                            name = input.nextLine();
-                            /* Procura o nome no Arraylist de novo. Ainda não
-                            sei como.
-                            
-                            for(Vehicles v: name.vehiclesOfCostumer){
-                                rel = v.gerarRelatorio();
-                                System.out.println(rel);
-                            }*/
-                            break;
-                        case 0:
-                            answer = 9;
-                            break;
-                        default:
-                            System.out.println("Opcao invalida.");
-                    }
+                    volta = c.menuVeiculo(people);
+                    if(volta == true)
+                        answer = 10;
                     break;
                 case 3: //Gerenciamento de serviços
-                    /* Podemos ter duas opções aqui, realizar serviços e
-                    checar insumos, ou 2 cases separados no menu
-                    principal. */
-                    answer = 9;
+                    /* Alguns dos itens daqui vão ser "estimativa de tempo de
+                    espera" e "diagnóstico do veículo e serviços futuros" e
+                    "geração de relatórios".
+                    Quando for lavar o veículo pode ter algo como
+                    setFuncionario, onde selecionamos um aleatoriamente */
                     break;
                 case 4: //Gerenciamento de funcionários
-                    System.out.println("\tFUNCIONARIOS");
-                    System.out.println("(1) Cadastrar um novo funcionário");
-                    System.out.println("(2) Listar funcionários Cadastrados");
-                    System.out.println("(0) Voltar ao menu principal");
-                    answer2 = input.nextInt();
-                    switch(answer2){
-                        case 1:
-                            Employee p = new Employee(); 
-                            employees.add(p);
-                            System.out.println("Funcionario contratado com sucesso!");
-                            break;
-                        case 2:
-                            System.out.println("\tLISTA DE FUNCIONARIOS");
-                            for(Employee em : employees){
-                                rel = em.gerarRelatorio();
-                                System.out.println(rel);
-                            }
-                            break;
-                        case 0:
-                            answer = 9;
-                            break;
-                        default:
-                            System.out.println("Opcao invalida.");
-                    }
+                    volta = c.menuFuncionario(employees);
+                    if(volta == true)
+                        answer = 10;                    
                     break;
-                case 5:
-                    answer = 9;
+                case 5: //Gerenciamento de insumos
+                    
+                    break;
+                case 6:
+                    c.imprimeInfoEmpresa();
+                    answer = 10;
                     break;
                 default:
-                    System.out.println("\tMENU PRINCIPAL");
-                    /*Posteriormente pode ser o name_company*/
-                    System.out.println("(1) Clientes");
-                    System.out.println("(2) Veículos");
-                    System.out.println("(3) Serviços");
-                    System.out.println("(4) Funcionários");
-                    System.out.println("(5) Relatórios");
-                    System.out.println("(0) Sair");
-                    answer = input.nextInt();
+                    answer = c.menuPrincipal();
             }
-            System.out.println("");
         }while(answer > 0);
         
         //Salva os clientes e seus veículos em um arquivo e os empregados em outro
