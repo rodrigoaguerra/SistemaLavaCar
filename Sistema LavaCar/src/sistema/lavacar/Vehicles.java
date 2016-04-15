406b4f65fdd43a4b44f12d88203c93607854a8df
Gabriel Eugenio <gabriel.eug2@gmail.com>
2016-04-15 10:10:08 -0300

    Atributos private, setters e getters

--- Sistema LavaCar/src/sistema/lavacar/Vehicles.java
+++ Sistema LavaCar/src/sistema/lavacar/Vehicles.java
@@ -6,57 +6,90 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-public class Vehicles implements Serializable {
-    public String board, 
-                  brand,
-                  model,
-                  year,
-                  color,
-                  description,
-                  dateOfInsert;
-    public int  size;
+public class Vehicle implements Serializable {
+    private final String brand;
+    private final String model;
+    private final String board;
+    private final String year;
+    private String color;
+    private final String dateOfInsert;
+    private String description;
+    private final int size;
     
     SimpleDateFormat dateToday;
 
-    public Vehicles(){
-        Scanner input = new Scanner (System.in);
+    public Vehicle(){
+        Scanner input = new Scanner (System.in);  
+        
+        System.out.println("\tInformaçoes do veiculo");  
+        System.out.print("Marca .: "); 
+        brand = input.nextLine(); 
+          
+        System.out.print("Modelo .: "); 
+        model = input.nextLine(); 
+        
+        System.out.print("Placa .: "); 
+        board = input.nextLine(); 
+        
+        System.out.print("Ano .: "); 
+        year = input.nextLine(); 
+         
+        System.out.print("Cor .: "); 
+        color = input.nextLine(); 
+  
+        System.out.print("Decrição .: ");   
+        description = input.nextLine();   
+         
+        System.out.print("Tamanho (1 - Pequeno, 2 - Médio, 3 - Grande) .: ");   
+        size = input.nextInt();
+        input.nextLine(); //Tira o \n
 
-        System.out.println("\tInformaçoes do veiculo");
-        System.out.print("Placa .: ");
-        board = input.nextLine();
-
-        System.out.print("Marca .: ");
-        brand = input.nextLine();
-        
-        System.out.print("Modelo .: ");
-        model = input.nextLine();
-        
-        System.out.print("Ano .: ");
-        year = input.nextLine();
-        
-        System.out.print("Cor .: ");
-        color = input.nextLine();
-
-        System.out.print("Decrição .: ");  
-        description = input.nextLine();  
-        
-        System.out.print("Tamanho (1 - Pequeno, 2 - Médio, 3 - Grande) .: ");  
-        size = input.nextInt();  
-        
-        dateToday = new SimpleDateFormat("dd/MM/yyyy hh:mm");
-        dateOfInsert = dateToday.format(new Date()); 
+        dateToday = new SimpleDateFormat("dd/MM/yyyy hh:mm"); 
+        dateOfInsert = dateToday.format(new Date());
     }
     public String gerarRelatorio()
     {
         String rel = "";
-        /*nome = dono.getNome()? Vai depender de como é a associação entre
-        cliente e veículo.
-        rel = rel + "Proprietario: " + nome + "\n";
-        */
-        rel = rel + "Marca: " + brand + "\tModelo: " + model + "\n";
-        rel = rel + "Ano: " + year + "\tCor: " + color + "\n";
-        rel = rel + "Data de cadastro: " + dateOfInsert + "\n";
-        rel = rel + "Descrição: " + description + "\n";
+        rel = rel + "Marca: " + getBrand() + "\tModelo: " + getModel() + "\n";
+        rel = rel + "Placa: " + getBoard() + "\tAno: " + getYear() + "\tCor: " + getColor() + "\n";
+        rel = rel + "Data de cadastro: " + getDateOfInsert() + "\n";
+        rel = rel + "Descrição: " + getDescription() + "\n\n";
         return rel;
+    }
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    public String getBrand()
+    {
+        return brand;
+    }
+    public String getModel()
+    {
+        return model;
+    }
+    public String getBoard()
+    {
+        return board;
+    }
+    public String getYear()
+    {
+        return year;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+    public String getDateOfInsert()
+    {
+        return dateOfInsert;
+    }
+    public String getDescription()
+    {
+        return description;
     }
 }
