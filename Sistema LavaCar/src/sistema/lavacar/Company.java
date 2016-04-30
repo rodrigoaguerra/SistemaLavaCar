e0d9d90925b8ce981da0cc6c6828af926c1e6f1c
Gabriel Eugenio <gabriel.eug2@gmail.com>
2016-04-29 23:29:45 -0300

    Insumos e modificações nas construtoras
    - A estrutura foi levemente modificada. Agora a main somente chama o executar e este, por sua vez, chama os menus.
    - Adicionado o menuInsumos.
    - Construtoras agora recebem os valores como parâmetros.

--- Sistema LavaCar/src/sistema/lavacar/Company.java
+++ Sistema LavaCar/src/sistema/lavacar/Company.java
@@ -18,7 +18,8 @@ public class Company {
     private String address_company;
     private String phone_company;
     private String site_company;
-    private String cnpj; 
+    private String cnpj;
+    private Insumos insumos;
    // private String logo_company; Não seria slogan?
             
     public Company(String name, String adress, String phone,
@@ -29,24 +30,64 @@ public class Company {
         phone_company = phone;
         site_company = site;
         this.cnpj = cnpj;
+        insumos = new Insumos(100, 100, 100);
     }
-    public int menuPrincipal()
+    public void executar(ArrayList<Customer> customers,
+                         ArrayList<Employee> employees)
     {
         Scanner input = new Scanner(System.in);
-        System.out.println("\n\t" + name_company);
-        System.out.println("\tMENU PRINCIPAL");
-        System.out.println("(1) Clientes");
-        System.out.println("(2) Veículos");
-        System.out.println("(3) Serviços");
-        System.out.println("(4) Funcionários");
-        System.out.println("(5) Insumos");
-        System.out.println("(6) Informações da compania");
-        System.out.println("(0) Sair");
-        int answer = input.nextInt();
-        input.nextLine(); //Tira o \n
-        return answer;
+        int answer = 0; //Reposta do menu principal
+        boolean querVoltar=false;
+        do{
+            switch(answer){
+                case 1:
+                    querVoltar = menuCliente(customers);
+                    break;
+                case 2: 
+                    querVoltar = menuVeiculo(customers);
+                    break;
+                case 3: 
+                    /* "Executar"
+                    "estimativa de tempo de espera"
+                    "diagnóstico do veículo e serviços futuros" */
+                    break;
+                case 4:
+                    querVoltar = menuFuncionario(employees);
+                    break;
+                case 5:
+                    querVoltar = menuInsumos();
+                    break;
+                case 6:
+                    /* Vai precisar conhecer a classe finanças
+                    {to be created) para ver os lucros e gastos
+                    "Diário"
+                    "Mensal"
+                    "Anual" */
+                    break;
+                case 7:
+                    imprimeInfoEmpresa();
+                    answer = 10;
+                    break;
+                default:
+                    System.out.println("\n\t" + name_company);
+                    System.out.println("\tMENU PRINCIPAL");
+                    System.out.println("(1) Clientes");
+                    System.out.println("(2) Veículos");
+                    System.out.println("(3) Serviços");
+                    System.out.println("(4) Funcionários");
+                    System.out.println("(5) Insumos");
+                    System.out.println("(6) Relatório");
+                    System.out.println("(7) Informações da compania");
+                    System.out.println("(0) Sair");
+                    answer = input.nextInt();
+                    input.nextLine(); //Tira o \n
+            }
+            if(querVoltar == true)
+                answer = 10;   
+        }while(answer > 0);
+        System.out.println("Saindo...");
     }
-    public boolean menuCliente(ArrayList<Customer> people)
+    public boolean menuCliente(ArrayList<Customer> customers)
     {
         Scanner input = new Scanner(System.in);
         System.out.println("\tCLIENTES");
@@ -57,25 +98,52 @@ public class Company {
         int answer = input.nextInt();
         input.nextLine(); //Tira o \n
         
+        String name, address, phone1, phone2, rel;
         switch(answer){
-            case 1:
-                Customer person = new Customer(); 
-                people.add(person);
+            case 1:        
+                System.out.print("Nome .: ");
+                name = input.nextLine();
+
+                System.out.print("RG .: ");
+                String rg = input.nextLine();
+
+                System.out.print("CPF .: ");
+                String cpf = input.nextLine();
+
+                System.out.print("Data de nascimento (DD MM AA).: ");   
+                int dia = input.nextInt();
+                int mes = input.nextInt();
+                int ano = input.nextInt();
+                input.nextLine(); //Tira o \n
+                /* É melhor como int para poder verificar se é válida */
+                String dateOfBirth = dia + "/" + mes + "/" + ano;
+
+                System.out.print("Endereço .: ");
+                address = input.nextLine();
+
+                System.out.print("Telefone1 .: ");
+                phone1 = input.nextLine();
+
+                System.out.print("Telefone2 .: ");
+                phone2 = input.nextLine();
+                
+                Customer person = new Customer(name, rg, cpf, dateOfBirth,
+                                              address, phone1, phone2); 
+                customers.add(person);
                 System.out.println("Cliente cadastrado com sucesso!\n");
                 break;
             case 2:
                 System.out.println("\tLISTA DE CLIENTES");
-                for(Customer p : people)
+                for(Customer p : customers)
                 {
-                    String rel = p.gerarRelatorio();
+                    rel = p.gerarRelatorio();
                     System.out.println(rel);
                 }
                 break;
             case 3:
                 System.out.print("Digite o nome do cliente: ");
-                String name = input.nextLine();
-                String address, phone1, phone2;
-                for(Customer p : people)
+                name = input.nextLine();
+                for(Customer p : customers)
                     if(name.equals(p.getName()))
                     {
                         System.out.print("Digite o endereço: ");
@@ -96,7 +164,7 @@ public class Company {
         }
         return false;
     }
-    public boolean menuVeiculo(ArrayList<Customer> people)
+    public boolean menuVeiculo(ArrayList<Customer> customers)
     {
         Scanner input = new Scanner(System.in);
         System.out.println("\tVEICULOS");
@@ -113,7 +181,7 @@ public class Company {
             case 1:                
                 System.out.print("Digite o nome do proprietario: ");
                 name = input.nextLine();
-                for(Customer p : people)
+                for(Customer p : customers)
                     if(name.equals(p.getName()))
                     {
                         p.adicionaVeiculo();
@@ -123,7 +191,7 @@ public class Company {
                 break;
             case 2:
                 System.out.println("\tLISTA DE VEICULOS");
-                for(Customer p : people)
+                for(Customer p : customers)
                 {
                     System.out.println("Proprietario: " + p.getName());
                     for(Vehicle v : p.vehiclesOfCustomer)
@@ -140,7 +208,7 @@ public class Company {
                 /*  Procura o cliente no ArrayList e mostra as
                     informações do(s) seu(s) veículo(s) */
                 System.out.println("\tVEICULOS DE " + name);
-                for(Customer p : people)
+                for(Customer p : customers)
                     if(name.equals(p.getName()))
                     {
                         for(Vehicle v : p.vehiclesOfCustomer)
@@ -155,7 +223,7 @@ public class Company {
                 System.out.print("Digite a placa do veículo a ser alterado: ");
                 String board = input.nextLine();
                 String color, description;
-                for(Customer p : people)
+                for(Customer p : customers)
                     for(Vehicle v : p.vehiclesOfCustomer)
                         if(board.equals(v.getBoard()))
                         {
@@ -185,10 +253,40 @@ public class Company {
         int answer = input.nextInt();
         input.nextLine(); //Tira o \n
         
-        String rel;
+        String name, address, phone1, phone2, rel;
         switch(answer){
             case 1:
-                Employee person = new Employee(); 
+                System.out.print("Nome .: ");
+                name = input.nextLine();
+
+                System.out.print("RG .: ");
+                String rg = input.nextLine();
+
+                System.out.print("CPF .: ");
+                String cpf = input.nextLine();
+
+                System.out.print("Data de nascimento (DD MM AA).: ");   
+                int dia = input.nextInt();
+                int mes = input.nextInt();
+                int ano = input.nextInt();
+                input.nextLine(); //Tira o \n
+                /* É melhor como int para poder verificar se é válida */
+                String dateOfBirth = dia + "/" + mes + "/" + ano;
+
+                System.out.print("Endereço .: ");
+                address = input.nextLine();
+
+                System.out.print("Telefone1 .: ");
+                phone1 = input.nextLine();
+
+                System.out.print("Telefone2 .: ");
+                phone2 = input.nextLine();
+
+                System.out.print("Salario .; ");
+                double payment = input.nextDouble();
+                
+                Employee person = new Employee(name, rg, cpf, dateOfBirth,
+                                              address, phone1, phone2, payment); 
                 employees.add(person);
                 System.out.println("Funcionario contratado com sucesso!");
                 break;
@@ -201,8 +299,7 @@ public class Company {
                 break;
             case 3:
                 System.out.print("Digite o nome do funcionário: ");
-                String name = input.nextLine();
-                String address, phone1, phone2;
+                name = input.nextLine();
                 for(Employee p : employees)
                     if(name.equals(p.getName()))
                     {
@@ -217,6 +314,39 @@ public class Company {
                         p.setPhone2(phone2);
                     }
                 break;                
+            case 0:
+                return true;
+            default:
+                System.out.println("Opcao invalida.");
+        }
+        return false;
+    }
+    public boolean menuInsumos()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\tINSUMOS");
+        System.out.println("(1) Checar");
+        System.out.println("(2) Adicionar");
+        System.out.println("(0) Voltar ao menu principal");
+        int answer = input.nextInt();
+        input.nextLine();
+        
+        switch(answer)
+        {
+            case 1:
+                System.out.println("Sabao: " + insumos.getSabao());
+                System.out.println("Esponjas: " + insumos.getEsponjas());
+                System.out.println("Panos: " + insumos.getPanos());
+                break;
+            case 2:
+                System.out.println("Quantidade de sabao comprado: ");
+                int sabao = input.nextInt();
+                System.out.println("Quantidade de esponjas compradas: ");
+                int esponjas = input.nextInt();
+                System.out.println("Quantidade de panos comprados: ");
+                int panos = input.nextInt();
+                insumos.adicionar(sabao, esponjas, panos);
+                break;
             case 0:
                 return true;
             default:
