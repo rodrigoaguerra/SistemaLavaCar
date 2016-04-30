fa21dfe74a5473d564842fa353acf89805ad2eae
Gabriel Eugenio <gabriel.eug2@gmail.com>
2016-04-29 23:42:17 -0300

    Empresa fixa
    - Com a empresa fixa, temos menos informações para digitar no começo do programa. Faz mais sentido do que perguntar toda vez que o programa é executado.
    - Já que o ArrayList de Employee se chama "employees", nada mais justo do que o de Customer se chamar "customers".
    
    PS: Podem mudar a empresa (nome, endereço, etc) se quiserem, eu coloquei o que passou pela minha cabeça na hora.

--- Sistema LavaCar/src/sistema/lavacar/SistemaLavaCar.java
+++ Sistema LavaCar/src/sistema/lavacar/SistemaLavaCar.java
@@ -16,68 +16,21 @@ import java.util.Scanner;
 public class SistemaLavaCar {
     public static void main(String[] args) throws FileNotFoundException{
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("\tINFORMACOES DA EMPRESA");
-        System.out.print("Nome: ");
-        String name = input.nextLine();
-        System.out.print("Endereço: ");
-        String address = input.nextLine();
-        System.out.print("Telefone: ");
-        String phone = input.nextLine();
-        System.out.print("Site: ");
-        String site = input.nextLine();
-        System.out.print("CNPJ: ");
-        String cnpj = input.nextLine();
 
-        Company c = new Company(name, address, phone, site, cnpj);
+        Company c = new Company("Lava Car T", "Rua Marechal Floriano, 57",
+                            "3089-1452", "www.lavacart.com", "71.546.945/0001-53");
                         
-        ArrayList<Customer> people = new ArrayList<>();
+        ArrayList<Customer> customers = new ArrayList<>();
         ArrayList<Employee> employees  = new ArrayList<>();
         
-        // Lê as informações dos clientes, funcionários
-        people = (ArrayList<Customer>) WorkingFile.read(people, "customers");
+        // Lê as informações dos clientes e funcionários
+        customers = (ArrayList<Customer>) WorkingFile.read(customers, "customers");
         employees = (ArrayList<Employee>) WorkingFile.read(employees, "employees");
         
-        int answer = 0; //Reposta do menu principal
-        boolean volta;
-        do{
-            switch(answer){
-                case 1 : // Gerenciamento de clientes
-                    volta = c.menuCliente(people);
-                    if(volta == true)
-                        answer = 10;
-                    break;
-                case 2: //Gerenciamento de veículos
-                    volta = c.menuVeiculo(people);
-                    if(volta == true)
-                        answer = 10;
-                    break;
-                case 3: //Gerenciamento de serviços
-                    /* Alguns dos itens daqui vão ser "estimativa de tempo de
-                    espera" e "diagnóstico do veículo e serviços futuros" e
-                    "geração de relatórios".
-                    Quando for lavar o veículo pode ter algo como
-                    setFuncionario, onde selecionamos um aleatoriamente */
-                    break;
-                case 4: //Gerenciamento de funcionários
-                    volta = c.menuFuncionario(employees);
-                    if(volta == true)
-                        answer = 10;                    
-                    break;
-                case 5: //Gerenciamento de insumos
-                    
-                    break;
-                case 6:
-                    c.imprimeInfoEmpresa();
-                    answer = 10;
-                    break;
-                default:
-                    answer = c.menuPrincipal();
-            }
-        }while(answer > 0);
+        c.executar(customers, employees);
         
         //Salva os clientes e seus veículos em um arquivo e os empregados em outro
-        WorkingFile.write(people, "customers");
+        WorkingFile.write(customers, "customers");
         WorkingFile.write(employees, "employees");
     }
 }
