cf5c865ae514609a96892f2fb0850f60851407f4
Gabriel Eugenio <gabriel.eug2@gmail.com>
2016-05-20 09:57:54 -0300

    Coméntário do NetBeans sobre licença retirado

--- Sistema LavaCar/src/sistema/lavacar/ProjetoLavaCar.java
+++ Sistema LavaCar/src/sistema/lavacar/ProjetoLavaCar.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistema.lavacar;
 
 import java.io.FileNotFoundException;
 
-/**
- *
- * @author Rodrigo Alves Guerra, Gabriel Eugenio Brito, Caio
- */
 public class ProjetoLavaCar {
     public static void main(String[] args) throws FileNotFoundException{
 
