97986cdb4593cca93901753c2846a656c740f551
Gabriel Eugenio <gabriel.eug2@gmail.com>
2016-05-24 17:59:31 -0300

    Cria o objeto Interf que a Company vai conhecer

--- Sistema LavaCar/src/sistema/lavacar/ProjetoLavaCar.java
+++ Sistema LavaCar/src/sistema/lavacar/ProjetoLavaCar.java
@@ -4,9 +4,10 @@ import java.io.FileNotFoundException;
 
 public class ProjetoLavaCar {
     public static void main(String[] args) throws FileNotFoundException{
-
+        UserInterface interf = new UserInterface();
         Company c = new Company("Lava Car T", "Rua Marechal Floriano, 57",
-                            "3089-1452", "www.lavacart.com", "71.546.945/0001-53");
+                            "3089-1452", "www.lavacart.com", "71.546.945/0001-53",
+                             interf);
         
         c.executar();
     }
